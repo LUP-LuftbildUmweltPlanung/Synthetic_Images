497c902301e2e0de4604623ea0c905052b8c6720
QuadroRTX <luftbildumweltplanung@gmail.com>
2021-11-12 15:04:07 +0100

    Added Place_tree function
    Added place tree function, as well as mask drawing. Trees can be placed with rectangular distance area

--- main.py
+++ main.py
@@ -1,65 +1,40 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 import synth_forest as forest
-from utils import load_image, save_image
+from utils import save_image
 
 # CONFIG #
 background_file = r'Example_Tree_Data/Background/test_background_8bit.tif'
 trees_path = r'Example_Tree_Data/8bit'
-number_of_trees = 100
+number_of_trees = 10
+distance = 50
 
-background, mask, blocked_area = forest.set_background(background_file)
+background, mask, free_area = forest.set_background(background_file)
 limits = background.shape
-trees = forest.get_trees(trees_path)
+trees, type_to_number, number_to_type = forest.get_trees(trees_path)
 
 for i in range(number_of_trees):
-    # loop to add trees
-    x = np.random.choice(limits[0])  # x_position for tree
-    y = np.random.choice(limits[1])  # y_position for tree
+    background, mask, free_area, full = forest.place_tree(distance, trees, background, mask, free_area, type_to_number)
+    if full == 1:
+        break
 
-    img_idx = np.random.choice(len(trees))  # random tree
-    img = load_image(trees[img_idx][0])
-
-    img_shape = img.shape  # gets image shape
-    img_center = (int(img_shape[0] / 2), int(img_shape[1] / 2))  # gets image center
-
-    x_range = [x - img_center[0],
-               x - img_center[0] + img_shape[0]]  # gets x_range according do image shape and x position
-    y_range = [y - img_center[1],
-               y - img_center[1] + img_shape[1]]  # gets y_range according do image shape and y position
-
-    if x_range[0] < 0:  # checks, if image is out of left bound
-        overlap = -1 * x_range[0]
-        img = img[overlap:]
-    elif x_range[1] > limits[0]:  # checks, if image is out of right bound
-        overlap = x_range[1] - limits[0]
-        img = img[:-overlap]
-
-    if y_range[0] < 0:  # checks, if image is out of upper bound
-        overlap = -1 * y_range[0]
-        img = img[:, overlap:]
-    elif y_range[1] > limits[1]:  # checks, if image is out of lower bound
-        overlap = y_range[1] - limits[1]
-        img = img[:, :-overlap]
-
-    img_mask = img == 0  # mask to only remove tree part of image
-
-    x_range = np.clip(x_range, 0, limits[0])  # clip x_range to image bounds
-    y_range = np.clip(y_range, 0, limits[1])  # clip y_range to image bounds
-
-    background[x_range[0]:x_range[1], y_range[0]:y_range[1]] *= img_mask  # empty tree area in background
-    background[x_range[0]:x_range[1], y_range[0]:y_range[1]] += img  # add tree into freshly deleted area
+distance = 10
+forest.fill_with_trees(distance, trees, background, mask, free_area, type_to_number)
 
 plt.imshow(background)
 plt.show()
 
+plt.imshow(mask, cmap='hot')
+plt.show()
+
+# plt.imshow(free_area * 255, cmap='gray', vmin=0, vmax=255)
+# plt.show()
+
 # save_image(r'Example_Tree_Data/8bit/test_background_8bit_synth.tif', background)
 
-# notes:    add (basic) tree augmentations, add tree type counter
-#           update image storage to image path storage
-# functions:    add single tree (needs a somewhat randomized area around it, where no trees can be placed)
-#                   param: tree-area
-#               add tree cluster (trees need a smaller of limits area, so they are not inside each other)
+# notes:    add (basic) tree augmentations, add tree type counter, replace square with circle (see google)
+#           move useful parts of "place_tree" to utils
+# functions:    add tree cluster (trees need a smaller of limits area, so they are not inside each other)
 #                   param: tree-distance, tree-amount
-#               add fill function (places trees until all areas outside of tree-area contain a tree)
+#
+# issues:   if tree contains black pixels (value=0), mask contains holes accordingly
--- synth_forest.py
+++ synth_forest.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 from utils import get_files, load_image
 
 
@@ -13,9 +12,9 @@ def set_background(file_path, reset=True, bands=None):
     """
     background = load_image(file_path, bands)
     if reset:
-        mask = np.zeros_like(background)
-        blocked_area = np.zeros_like(background)
-        return background, mask, blocked_area
+        mask = np.zeros_like(background[:, :, 0])
+        free_area = np.ones_like(background[:, :, 0])
+        return background, mask, free_area
     else:
         return background
 
@@ -31,9 +30,100 @@ def get_trees(files_path, file_type=None):
     if file_type is None:
         file_type = 'tif'
     files = get_files(files_path, file_type)
-    trees = {}
-    for idx, file in enumerate(files):
-        tree_type = str(file).rsplit('_', 1)[0]
-        trees[idx] = (file, tree_type)
+    trees = []
+    tree_types = ['background']
+    for file in files:
+        tree_type = str(file).rsplit('\\', 1)[1].rsplit('_', 1)[0]
+        trees.append((file, tree_type))
+        if tree_type not in tree_types:
+            tree_types.append(tree_type)
+
+    tree_labels = np.arange(len(tree_types), dtype='uint8')
+    type_to_number = dict(zip(tree_types, tree_labels))
+    number_to_type = dict(zip(tree_labels, tree_types))
+    print(type_to_number)
+    print(number_to_type)
+
     print(f'Successfully loaded {len(trees)} tree images of type {file_type}.')
-    return trees
+    return trees, type_to_number, number_to_type
+
+
+def place_tree(distance, trees, background, mask, free_area, type_to_number):
+    """Places a single tree in a given distance of all other trees, updates the image mask and free area respectively.
+
+            Keyword arguments:
+            distance -- distance in pixels to be blocked around each tree
+            trees -- list containing tuples of type (tree_image_path, tree_type)
+            background -- array containing the image (N-dimensional)
+            mask -- array containing the image mask (1-dimensional)
+            free_area -- array containing 1 where trees can be placed (1-dimensional)
+            type_to_number -- dictionary mapping tree type to numerical label
+    """
+    if np.sum(free_area) == 0:
+        print('Image does not contain any free area anymore. No tree was placed.')
+        return background, mask, free_area, 1
+
+    area_likelihoods = free_area / np.sum(free_area != 0)  # calculates likelihood for each position
+    pos = np.random.choice(np.arange(free_area.size), p=area_likelihoods.flatten())  # selects position in 1D image
+
+    x = int(pos / free_area.shape[1])  # calculates true x_position for 2D position from 1D position
+    y = int(pos % free_area.shape[1])  # calculates true y_position for 2D position from 1D position
+
+    tree_idx = np.random.choice(len(trees))  # selects random tree
+    tree = load_image(trees[tree_idx][0])  # gets image
+    tree_type = trees[tree_idx][1]  # gets tree type
+    tree_label = type_to_number[tree_type]  # converts tree_type to label
+
+    tree_shape = tree.shape  # gets image shape
+    tree_center = (int(tree_shape[0] / 2), int(tree_shape[1] / 2))  # gets image center
+
+    x_range = [x - tree_center[0],
+               x - tree_center[0] + tree_shape[0]]  # gets x_range according do image shape and x position
+    y_range = [y - tree_center[1],
+               y - tree_center[1] + tree_shape[1]]  # gets y_range according do image shape and y position
+
+    limits = background.shape
+
+    if x_range[0] < 0:  # checks, if image is out of left bound
+        overlap = -1 * x_range[0]
+        tree = tree[overlap:]
+    elif x_range[1] > limits[0]:  # checks, if image is out of right bound
+        overlap = x_range[1] - limits[0]
+        tree = tree[:-overlap]
+
+    if y_range[0] < 0:  # checks, if image is out of upper bound
+        overlap = -1 * y_range[0]
+        tree = tree[:, overlap:]
+    elif y_range[1] > limits[1]:  # checks, if image is out of lower bound
+        overlap = y_range[1] - limits[1]
+        tree = tree[:, :-overlap]
+
+    tree_mask = tree != 0  # mask to only remove tree part of image
+
+    x_range = np.clip(x_range, 0, limits[0])  # clip x_range to image bounds
+    y_range = np.clip(y_range, 0, limits[1])  # clip y_range to image bounds
+
+    background[x_range[0]:x_range[1], y_range[0]:y_range[1]] *= tree_mask == 0  # empties tree area in background
+    background[x_range[0]:x_range[1], y_range[0]:y_range[1]] += tree  # adds tree into freshly deleted area
+
+    mask[x_range[0]:x_range[1], y_range[0]:y_range[1]] *= tree_mask[:, :, 0] == 0  # empties tree area in mask
+    mask[x_range[0]:x_range[1], y_range[0]:y_range[1]] += tree_mask[:, :, 0] * tree_label  # adds tree mask
+
+    x_block_range = np.clip([x - distance, x + distance], 0, limits[0])  # calculates blocked area from distance
+    y_block_range = np.clip([y - distance, y + distance], 0, limits[1])
+
+    free_area[x_block_range[0]:x_block_range[1], y_block_range[0]:y_block_range[1]] = 0  # sets blocked area
+
+    return background, mask, free_area, 0
+
+
+def fill_with_trees(distance, trees, background, mask, free_area, type_to_number):
+    fill = 0
+    counter = 0
+    while fill == 0:
+        background, mask, free_area, fill = place_tree(distance, trees, background, mask, free_area, type_to_number)
+        counter += 1
+        if counter % 50 == 0:
+            print(f'{counter} trees placed.')
+
+    print(f'Forest has been filled. A total of {counter} additional trees have been placed.')
